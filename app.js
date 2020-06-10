11a88a75b8e1c7142c278293870dd7b9a8cabe2e
Dung <dung.dao19@outlook.com>
2020-06-10 18:30:40 -0500

    second commit

--- app.js
+++ app.js
@@ -9,3 +9,40 @@ function isTouching(a, b) {
 		aRect.left > bRect.left + bRect.width
 	);
 }
+
+const avatar = document.querySelector('#player');
+const coin = document.querySelector('#coin');
+
+window.addEventListener('keyup', (e) => {
+	if (e.key === 'ArrowDown' || e.key === 'Down') {
+		const currentTop = extractPos(avatar.style.top);
+		avatar.style.top = `${currentTop + 50}px`
+	} else if (e.key === 'ArrowUp' || e.key === 'Up') {
+		const currentTop = extractPos(avatar.style.top);
+		avatar.style.top = `${currentTop - 50}px`
+	} else if (e.key === 'ArrowRight' || e.key === 'Right'){
+		const currentLeft = extractPos(avatar.style.left);
+		avatar.style.left = `${currentLeft + 50}px`
+		avatar.style.transform = 'scale(1,1)'
+	} else if (e.key === 'ArrowLeft' || e.key === 'Left'){
+		const currentLeft = extractPos(avatar.style.left);
+		avatar.style.left = `${currentLeft - 50}px`
+		avatar.style.transform = 'scale(-1,1)'
+	}
+	if(isTouching(avatar, coin)) moveCoin();
+
+});
+
+const extractPos = (pos) => {
+	if (!pos) return 100;
+	return parseInt(pos.slice(0,-2));
+}
+
+const moveCoin = () => {
+	const x = Math.floor(Math.random() * window.innerHeight);
+	const y = Math.floor(Math.random() * window.innerWidth);
+	coin.style.top = `${y}px`
+	coin.style.left = `${x}px`
+}
+
+moveCoin();
